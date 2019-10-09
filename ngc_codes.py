7dd97f8b7587a1bdeb998f6d44fbcff1e21fc0e6
Stef Marais <stephan3m@gmail.com>
2019-10-09 15:13:38 +0800

    initial commit

---
+++ ngc_codes.py
@@ -0,0 +1,16 @@
+ngc_object = {
+    "?Q100": "SERIAL NUMBER, 1234567",
+    "?Q101": "SOFTWARE VERSION, 100.17.000.2037",
+    "?Q102": "MODEL, CSMD-G2",
+    "?Q104": "MODE, ZERO",
+    "?Q200": "TOOL CHANGES, 35",
+    "?Q201": "USING TOOL, 4",
+    "?Q300": "P.O. TIME, 06282:17:13",
+    "?Q301": "C.S. TIME, 00098:18:29",
+    "?Q303": "LAST CYCLE, 00000:00:13",
+    "?Q304": "PREV CYCLE, 00000:00:01",
+    "?Q402": "M30 #1, 380",
+    "?Q403": "M30 #2, 380",
+    "?Q500": "Oxxxxx, STATUS, PARTS, xxxxx PROGRAM, MDI, IDLE, PARTS, 380",
+    "?Q600": "MACRO, 0.0"
+}
